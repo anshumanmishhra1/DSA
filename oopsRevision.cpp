652d9d2569122109329ae3b254fba3f716bc8d9d
Anshuman <anshumanmsihhra1@gmail.com>
2023-10-17 14:42:52 +0530

    17 oct

--- oopsRevision.cpp
+++ oopsRevision.cpp
@@ -1,14 +1,14 @@
-/* Concept Of getter and setter and public and private class: */
+// /* Concept Of getter and setter and public and private class: */
 
 // #include<iostream>
 // using namespace std;
 // class hero{
     
 //     private:
-//     char name;
-//     char level;
 //     int health;
 //     public:
+//     char name;
+//     char level;
 //     int defence;
 //     char getLevel(){
 //         return level;
@@ -35,30 +35,92 @@
 //     }
 // };
 // int main(){
-//     hero ramesh;
-//     cout<<"Ramesh health is: "<<ramesh.getHealth()<<endl;
-//     ramesh.setHealth(70);
-//     // cout<<"size of name: "<<sizeof(ramesh.name);
+//     // hero ramesh;
+//     // cout<<"Ramesh health is: "<<ramesh.getHealth()<<endl;
+//     // ramesh.setHealth(70);
+
+//     /* Static Vs Dynamic Memory allocation :
+//     Static:
+//         hero a;
+    
+//         cout<<"level is : "<< a.level<<endl;
+//         cout<<"health is : "<< a.getHealth();
+    
+//     Dynamic:
+//         hero *b= new hero;
+//  */
+
+//     // hero *b=new hero;
+//     // b->setLevel('A');
+//     // b-> setHealth(60);
+//     // cout<<"Level is :"<<(*b).level<<endl<<endl;
+//     // cout<<"Level is :"<<(*b).getHealth()<<endl;
+
+//     // //we can also use -> for refering and printing:
+    
+//     // cout<<"Level is :"<<b->level<<endl;
+//     // cout<<"Level is :"<<b->getHealth()<<endl;
+
+    
+
 //     return 0;
 
 // }
 
-/* Write a c++ prograam to find the sum of n naturral nummvers using recurssion: */
 
-// #include<iostream>
-// using namespace std;
-// int sum(int n){
-//     if(n==0){
-//         return;
-//     }
 
-//     int i=0;
-//     return i+sum(n-1);
-// }
-// int main(){
-//     int n;
-//     cin>>n;
-//     sum(n);
-//     return 0;
 
-// }
+
+
+/* Concept of Constructor:
+    1. It is used while object creation
+    2. No return type
+    3. there is no input parameter
+
+    -> whenever we write Hero ramesh; it means ramesh.hero() is calling
+    and this is known as default constructor.
+
+*/
+#include<iostream>
+using namespace std;
+class hero{
+    
+    private:
+    int health;
+    public:
+    char level;
+    
+    hero(){
+        cout<<"Constructor called : "<<endl;
+    }
+    char getLevel(){
+        return level;
+    }
+
+
+    int getHealth(){
+        return health;
+    }
+
+    char setLevel(char ch){
+        level=ch;
+    }
+
+  
+
+    void setHealth(int h){
+        health=h;
+    }
+};
+
+int main(){
+    //object created statically
+    // cout<<"Hii"<<endl;
+    hero ramesh;
+    // cout<<"hello"<<endl;
+
+
+    //object created dynamically
+    hero *h= new hero;
+    return 0;
+}
