3656f12554532dc9d3130ee9dfc862ca03062d96
Anshuman <anshumanmsihhra1@gmail.com>
2023-10-18 13:59:22 +0530

    ho

--- recurrsionRevison.cpp
+++ recurrsionRevison.cpp
@@ -45,4 +45,26 @@
 // }
 
 
+
 /* Write a function to calculate the nth fibonaaci number using Recurssion: */
+
+// #include<iostream>
+// using namespace std;
+// int fibo(int n){
+//     if(n==1 || n==2){
+//         return 1;
+//     }
+//     return fibo(n-1)+fibo(n-2);
+// }
+// int main(){
+//     int n;
+//     cout<<"Enter the number: "<<endl;
+//     cin>>n;
+//     cout<<fibo(n);
+//     return 0;
+
+// }
+
+
+
+
