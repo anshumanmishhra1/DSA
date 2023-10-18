9cc15ac2c732af3ddf52fab6d32286f79f7014a3
Anshuman <anshumanmsihhra1@gmail.com>
2023-10-18 13:15:45 +0530

    recurssion revision:

---
+++ recurrsionRevison.cpp
@@ -0,0 +1,48 @@
+/* Print decreasing and increasing of n*/
+
+// #include<iostream>
+// using namespace std;
+// int IncDec(int n){
+//     if(n==0){
+//         return 1;
+//     }
+//     cout<<n<<endl;
+//     IncDec(n-1);
+//     cout<<n<<endl;
+
+// }
+// int main(){
+//     int n;
+//     cout<<"enter the value of n: "<<endl;
+//     cin>>n;
+//     IncDec(n);
+
+//     return 0;
+
+// }
+
+
+/* Print Power of a^b */
+
+// #include<iostream>
+// using namespace std;
+// int power(int a, int b){
+//     if(b==0){
+//         return 1;
+//     }
+//     return a* power(a,b-1);
+// }
+// int main(){
+//     int a,b;
+//     cout<<"Enter the value of A: "<<endl;
+//     cin>>a;
+//     cout<<"Enter the value of B: "<<endl;
+//     cin>>b;
+//     int ans=power(a,b);
+//     cout<<"Power of : " <<a<< " to the power of : "<<b<< " is : "<<ans;
+//     return 0;
+
+// }
+
+
+/* Write a function to calculate the nth fibonaaci number using Recurssion: */
