80c08e9e5740b5c99fcbbb2c860ad39b406c4564
Anshuman <anshumanmsihhra1@gmail.com>
2023-10-18 11:44:54 +0530

    18oct 2023

--- oopsRevision.cpp
+++ oopsRevision.cpp
@@ -81,57 +81,64 @@
     and this is known as default constructor.
 
 */
-#include<iostream>
-using namespace std;
-class hero{
+// #include<iostream>
+// using namespace std;
+// class hero{
     
-    private:
-    int health;
-    public:
-    char level;
+//     private:
+//     int health;
+//     public:
+//     char *name;
+//     char level;
     
-    hero(){
-        cout<<"Constructor called : "<<endl;
-    }
-    char getLevel(){
-        return level;
-    }
+//     hero(){
+//         cout<<"Constructor called : "<<endl;
+//     }
+//     char getLevel(){
+//         return level;
+//     }
 
-    void print(){
-        cout<<"health"<< this->health<<endl;
-        cout<<"Level "<< this->level<<endl;
-    }
+//     void print(){
+//         cout<<endl;
+//         cout<<"Name: "<<this->name<<" ,";
+//         cout<<"health"<< this->health<<endl;
+//         cout<<"Level "<< this->level<<endl;
+//     }
 
-    int getHealth(){
-        return health;
-    }
+//     int getHealth(){
+//         return health;
+//     }
 
-    char setLevel(char ch){
-        level=ch;
-    }
+//     char setLevel(char ch){
+//         level=ch;
+//     }
 
-    hero(int health){ 
-        cout<<"this ->"<<this<<endl;
-        this -> health=health; 
-   }
+//     hero(int health){ 
+//         cout<<"this ->"<<this<<endl;
+//         this -> health=health; 
+//    }
 
-   hero(int health, char level){ 
-        this ->level=level;
-        this -> health=health; 
-   }
+//    hero(int health, char level){ 
+//         this ->level=level;
+//         this -> health=health; 
+//    }
     
-    // Here we are creating our own Copy Constructor: 
-    hero(hero&  temp){
-        this->health=temp.health;
-        this->level=temp.level;
-    }
+//     // Here we are creating our own Copy Constructor: 
+//     hero(hero&  temp){
+//         this->health=temp.health;
+//         this->level=temp.level;
+//     }
 
-    void setHealth(int h){
-        health=h;
-    }
-};
+//     void setHealth(int h){
+//         health=h;
+//     }
 
-int main(){
+//     void setName(char name[]){
+//         strcpy(this->name,name);
+//     }
+// };
+
+// int main(){
     /*
     //object created statically
     // cout<<"Hii"<<endl;
@@ -158,7 +165,7 @@ int main(){
 
     */
 
-   /* Concept of Copy Constructor: */
+   /* Concept of Copy Constructor: 
    hero S(70,'C');
    S.print();
 
@@ -168,6 +175,50 @@ int main(){
     // basically we mean of the above line that is written below: 
 //    R.health=suresh.health;
 //    R.level=suresh.level;
-   
+   */
+
+
+  /* Concept of shallow and deep copy */
+
+
+// }
+
+
+
+
+/* Understanding Concept of Constructor properly: */
+
+#include<iostream>
+using namespace std;
+class Rectangle{
+    public:
+    int l;
+    int b;
+    
+    Rectangle(){  //default constructor is called
+    l=0;
+    b=0;
+    }
+
+    Rectangle(int x, int y){ //parameterised construcot -args passed
+        l=x;
+        b=y;
+    }
+
+    Rectangle(Rectangle& r){ //copy constructor : initialise an objecct by another existing object
+        l=r.l;
+        b=r.b;
+    }
+}; 
+int main(){
+    Rectangle r1;
+    cout<<r1.l<<" "<< r1.b<<endl;
+
+    Rectangle r2(4,5);
+    cout<<r2.l<<" "<<r2.b<<endl;
+
+    Rectangle r3=r2;
+    cout<<r3.l<<" "<<r3.b<<endl;
+    return 0;
 
 }
